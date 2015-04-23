1191a454e03896d53ea6dfe130a34f28799fc47e
krisk1 <christian.kugler@stud.uni-regensburg.de>
2015-04-23 17:01:28 +0200

    fixed Jumpbug, cleanedup Code
    Jump jetzt auch von Kanten möglich, aber nur von flächen mit bestimmten
    Slope.
    Bugfixes: Jetzt auch turnen während Sprinten möglich

--- PSMG_SS_2015_Rats_Canalisation/Assets/Scripts/Rat/RatMovement.cs
+++ PSMG_SS_2015_Rats_Canalisation/Assets/Scripts/Rat/RatMovement.cs
@@ -5,40 +5,77 @@ public class RatMovement : MonoBehaviour {
 	
 	public float rotationSpeed = 1f;
 	public static float generalMovementSpeed = 3f;
+    public float runSpeed = 5f;
+    public float slowSpeed = 1f;
 	public float movementSpeed = generalMovementSpeed;
 	public float jumpSpeed = 20f;
 	public float RayCastLength = .1f;
-	public bool isGrounded;
+	public bool isGrounded = false;
+    //max Slope the Rat can jump of
+    public float maxSlope = 60f;
 	
 	void FixedUpdate()
 	{
 		float horizontalInput = Input.GetAxis("Horizontal");
-		float verticalInput = Input.GetAxis("Vertical");
+		float verticalInput = Input.GetAxis("Vertical");	
 		
-		if (Input.GetKeyDown(KeyCode.Space)&& isGrounded){
-			GetComponent<Rigidbody>().AddForce(Vector3.up * jumpSpeed);
-		}
-		
-		
-		checkIfGrounded();
+        Jump();
 		Move(verticalInput);
 		if (isGrounded)
-		{
 			Turn(horizontalInput);
-		}
-		
-		
+        Run();
+        Sneak();
+        NormalizeSpeed();
 	}
-	
-	
-	private void checkIfGrounded()
-	{
-		if (Physics.Raycast(transform.position, new Vector3(0, -.1f, 0), RayCastLength))
-		{
-			isGrounded = true;
-		}
-		else isGrounded = false;
-	}
+
+    private void Run()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            movementSpeed = runSpeed;
+        }
+    }
+
+    public void Sneak()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            movementSpeed = slowSpeed;
+        }
+    }
+
+    public void NormalizeSpeed()
+    {
+        if (Input.GetKeyUp(KeyCode.LeftControl) || Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            movementSpeed = generalMovementSpeed;
+        }
+    }
+
+    // sets isGrounded to true, if angle between the colliding vector and the upwards Vector is smaller than maxSlope
+    // determins weather ground is jumpable
+    public void OnCollisionStay(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            if (Vector3.Angle(contact.normal, Vector3.up) < maxSlope){
+                isGrounded = true;
+            }
+        }
+    }
+
+    public void OnCollisionExit()
+    {
+        isGrounded = false;
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * jumpSpeed);
+        }
+    }
 	
 	private void Move(float inputSignal)
 	{
@@ -61,19 +98,7 @@ public class RatMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		float runSpeed = 5f;
-		float slowSpeed = 1f;
-		//Run
-		if (Input.GetKeyDown (KeyCode.LeftControl)) {
-			movementSpeed = runSpeed;
-		} 
-		//Slow
-		if (Input.GetKeyDown (KeyCode.LeftShift)) {
-			movementSpeed = slowSpeed;
-		}
-		//Normalize
-		if (Input.GetKeyUp (KeyCode.LeftControl)||Input.GetKeyUp (KeyCode.LeftShift)) {
-			movementSpeed = generalMovementSpeed;
-		}
+		
+
 	}
 }
