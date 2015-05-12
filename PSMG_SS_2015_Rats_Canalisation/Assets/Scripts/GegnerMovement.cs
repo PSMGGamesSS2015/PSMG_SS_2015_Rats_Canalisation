075e5bfd38bf9cbae1119c835e4453012878a771
magnimma <marioseebauer@web.de>
2015-05-12 16:27:16 +0200

    Geheimen Gegner eingebaut, buggt aber noch etwas

---
+++ PSMG_SS_2015_Rats_Canalisation/Assets/Scripts/GegnerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class GegnerMovement : MonoBehaviour {
+
+	public float movementSpeed = 3f;
+	public float reactionDistance = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+		float dist = Vector3.Distance(player.transform.position, transform.position);
+		if (dist < reactionDistance) {
+			Vector3 newPosition2 = transform.forward.normalized * movementSpeed * Time.deltaTime;
+			transform.position = (transform.position + newPosition2);
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Player") {
+			other.transform.position = new Vector3(0, 0, 0);
+		}
+		
+	}
+}
