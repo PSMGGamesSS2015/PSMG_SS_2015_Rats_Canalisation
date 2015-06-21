e49161a3ef9b5346a2714dff60a386afe41e4581
magnimma <marioseebauer@web.de>
2015-06-21 07:03:00 +0200

    LightFlicker implemented

---
+++ PSMG_SS_2015_Rats_Canalisation/Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFlicker : MonoBehaviour {
+	public float timeOn = 0.1f;
+	public float timeOff = 0.5f;
+	private float changeTime = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Time.time > changeTime)
+		{ transform.GetComponent<Light>().enabled = !transform.GetComponent<Light>().enabled; 
+			if (transform.GetComponent<Light>().enabled) {
+				changeTime = Time.time + timeOn; 
+			}
+			else { 
+				changeTime = Time.time + timeOff; 
+			} 
+		} 
+	}
+}
