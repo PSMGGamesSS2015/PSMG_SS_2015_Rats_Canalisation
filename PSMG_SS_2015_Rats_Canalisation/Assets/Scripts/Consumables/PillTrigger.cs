5d2a4075e6fb368871c54f2fd4e129b7bee31365
magnimma <marioseebauer@web.de>
2015-06-21 10:10:53 +0200

    ragemode

---
+++ PSMG_SS_2015_Rats_Canalisation/Assets/Scripts/Consumables/PillTrigger.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PillTrigger : MonoBehaviour {
+	private Vector3 startPos; 
+	
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+		if (player.GetComponent<Attributes> ().diedcheck ()) {
+			playerDied();
+		}	
+	}
+	
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Player") {
+			GameObject.FindGameObjectWithTag("Player").GetComponent<RatMovement>().gotPill();
+			transform.position = new Vector3(0,-150,0);
+		}
+		
+	}
+	
+	void playerDied(){
+		transform.position = startPos;
+	}
+}
