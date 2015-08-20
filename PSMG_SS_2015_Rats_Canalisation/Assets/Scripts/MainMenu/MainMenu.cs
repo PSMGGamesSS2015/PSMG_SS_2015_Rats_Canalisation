826d2a496b9992e65d3183f76c795c5b199025ba
krisk1 <christian.kugler@stud.uni-regensburg.de>
2015-08-20 22:04:39 +0200

    Added Basics of Main Menu

---
+++ PSMG_SS_2015_Rats_Canalisation/Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    private Animator _animator;
+    private CanvasGroup _canvasgroup;
+
+
+    public bool isOpen
+    {
+        get { return _animator.GetBool("IsOpen"); }
+        set { _animator.SetBool("IsOpen", value);}
+    }
+	// Use this for initialization
+	void Awake () {
+        _animator = GetComponent<Animator>();
+        _canvasgroup = GetComponent <CanvasGroup>();
+
+        var rect = GetComponent<RectTransform>();
+        rect.offsetMax = rect.offsetMin = new Vector2(0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!_animator.GetCurrentAnimatorStateInfo(0).IsName("Open"))
+        {
+            _canvasgroup.blocksRaycasts = _canvasgroup.interactable = false;
+        }
+        else
+        {
+            _canvasgroup.blocksRaycasts = _canvasgroup.interactable = true;
+        }
+	}
+}
---
+++ PSMG_SS_2015_Rats_Canalisation/Assets/Scripts/MainMenu/MenuManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+    public MainMenu CurrentMenu;
+
+	// Use this for initialization
+	void Start () {
+        ShowMenu(CurrentMenu);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ShowMenu(MainMenu menu)
+    {
+        if (CurrentMenu != null)
+            CurrentMenu.isOpen = false;
+
+        CurrentMenu = menu;
+        CurrentMenu.isOpen = true;
+
+    }
+}
